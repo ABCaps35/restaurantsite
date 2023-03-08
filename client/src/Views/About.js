fdf27b90afd44f668484c65739683c775e9a59a3
ABCaps35 <acapelli345@gmail.com>
2023-03-07 20:39:16 -0500

    minor updates

--- client/src/Views/About.js
+++ client/src/Views/About.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Grid, Typography, Card, Box } from '@mui/material';
 
 const img_url = "https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg";
+const img_url2 = "https://a.cdn-hotels.com/gdcs/production0/d1513/35c1c89e-408c-4449-9abe-f109068f40c0.jpg?impolicy=fcrop&w=800&h=533&q=medium";
+const img_url3 = "https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg";
 
 const description1 = "Steve's is a delightful Italian restaurant that offers a warm and inviting atmosphere for diners. The restaurant's interior is decorated with classic Italian artwork and rustic furnishings that create a cozy and intimate ambiance. The menu at Steve's features an array of delicious Italian dishes that are made using only the freshest and finest ingredients. From homemade pastas to wood-fired pizzas, every dish is prepared with care and attention to detail. Whether you're in the mood for a traditional plate of spaghetti bolognese or a hearty meatball sub, Steve's has something for everyone. The restaurant's attentive and friendly staff provide excellent service, making for a truly memorable dining experience.";
 const description2 = "Steve's has been a beloved fixture in the town of Springfield, USA for over 30 years. The restaurant was founded by Steve, a first-generation Italian immigrant who had a passion for sharing his family's culinary traditions with the community. With hard work and dedication, Steve built a reputation for serving some of the best Italian food in the area. Over the years, the restaurant has become a local institution, attracting both loyal regulars and visitors who come from far and wide to sample its delicious cuisine.";
@@ -26,7 +28,7 @@ const About = (props) => {
                 <Card sx={{mt: 3}}>
                     <Grid container justifyContent='center' sx={{p:1}}>
                         <Grid item>
-                            <Box sx={{borderRadius: 2}} width="100%" component="img" src={img_url} alt={"Insert Image Here"}></Box>
+                            <Box sx={{borderRadius: 2}} width="100%" component="img" src={img_url2} alt={"Insert Image Here"}></Box>
                         </Grid>
                     </Grid>
                 </Card>
@@ -45,7 +47,7 @@ const About = (props) => {
                 <Card sx={{mt: 3}}>
                     <Grid container justifyContent='center' sx={{p:1}}>
                         <Grid item>
-                            <Box sx={{borderRadius: 2}} width="100%" component="img" src={img_url} alt={"Insert Image Here"}></Box>
+                            <Box sx={{borderRadius: 2}} width="100%" component="img" src={img_url3} alt={"Insert Image Here"}></Box>
                         </Grid>
                     </Grid>
                 </Card>
